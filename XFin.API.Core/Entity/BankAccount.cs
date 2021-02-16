b8678b8f6a41d70399c7b0f5128a6fe32ced039f
Patrick Hilgarth <philgarth@web.de>
2021-02-16 21:40:56 +0100

    Added .Core project with entities Depositor and BankAccount

---
+++ XFin.API.Core/Entity/BankAccount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace XFin.API.Core.Entities
+{
+    public class BankAccount
+    {
+        /*************************************************************************************************************
+         * 
+         * Public Members
+         * 
+        *************************************************************************************************************/
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("DepositorId")]
+        public int DepositorId { get; set; }
+        public Depositor Depositor { get; set; }
+
+        [Required]
+        public string AccountNumber { get; set; }
+
+        [Required]
+        public string Iban { get; set; }
+
+        [Required]
+        public string Bic { get; set; }
+
+        [Required]
+        public string Bank { get; set; }
+
+        [Required]
+        public string AccountType { get; set; }
+    }
+}
---
+++ XFin.API.Core/Entity/Depositor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace XFin.API.Core.Entities
+{
+    public class Depositor
+    {
+        /*************************************************************************************************************
+         * 
+         * Public Members
+         * 
+        *************************************************************************************************************/
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(25)]
+        public string Name { get; set; }
+
+        public ICollection<BankAccount> BankAccounts { get; set; }
+            = new List<BankAccount>();
+    }
+}
