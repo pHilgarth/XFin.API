51663eec5b7446a781fcaf12f8aa033b96b92196
Patrick Hilgarth <philgarth@web.de>
2021-11-07 20:17:27 +0100

    added entity InternalBankAccountSettings - not finished -  db migration still missing

---
+++ XFin.API.Core/Entities/InternalBankAccountSettings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace XFin.API.Core.Entities
+{
+    public class InternalBankAccountSettings
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("InternalBankAccountId")]
+        public int InternalBankAccountId { get; set; }
+        public InternalBankAccount InternalBankAccount { get; set; }
+
+        public bool EffectsExpenses { get; set; }
+        public bool ReceivesRevenues { get; set; }
+        public bool AllowsOverdraft { get; set; }
+        public decimal BalanceTreshold { get; set; }
+
+    }
+}
