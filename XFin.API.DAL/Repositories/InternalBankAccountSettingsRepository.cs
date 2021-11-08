dff0d96dc12ce5c1142dd614dfd279a037778ab3
Patrick Hilgarth <philgarth@web.de>
2021-11-08 19:20:55 +0100

    added InternalBankAccountSettingsRepository and -Profile for AutoMapper

---
+++ XFin.API.DAL/Repositories/InternalBankAccountSettingsRepository.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using System.Linq;
+using XFin.API.Core.Models;
+using XFin.API.DAL.DbContexts;
+using XFin.API.DAL.Interfaces;
+
+namespace XFin.API.DAL.Repositories
+{
+    public class InternalBankAccountSettingsRepository : IInternalBankAccountSettingsRepository
+    {
+        public InternalBankAccountSettingsRepository(XFinDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public InternalBankAccountSettingsModel GetInternalBankAccountSettings(int id)
+        {
+            var settings = context.InternalBankAccountSettings.Where(i => i.InternalBankAccountId == id).FirstOrDefault();
+
+            return settings != null
+                ? mapper.Map<InternalBankAccountSettingsModel>(settings)
+                : null;
+        }
+
+        private readonly XFinDbContext context;
+        private readonly IMapper mapper;
+    }
+}
---
+++ XFin.API/AutoMapperProfiles/InternalBankAccountSettingsProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using XFin.API.Core.Entities;
+using XFin.API.Core.Models;
+
+namespace XFin.API.AutoMapperProfiles
+{
+    public class InternalBankAccountSettingsProfile : Profile
+    {
+        public InternalBankAccountSettingsProfile()
+        {
+            CreateMap<InternalBankAccountSettings, InternalBankAccountSettingsModel>();
+        }
+    }
+}
